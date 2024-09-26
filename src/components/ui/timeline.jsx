a72e429cc793c4077ad3570cc9a781cfd08ccaca
radozeman <r.zeman96@gmail.com>
2024-09-27 01:39:27 +0200

    init

---
+++ src/components/ui/timeline.jsx
@@ -0,0 +1,70 @@
+"use client";
+import { useScroll, useTransform, motion } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+
+export const Timeline = ({ data }) => {
+  const ref = useRef(null);
+  const containerRef = useRef(null);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (ref.current) {
+      const rect = ref.current.getBoundingClientRect();
+      setHeight(rect.height);
+    }
+  }, [ref]);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start 10%", "end 50%"],
+  });
+
+  const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
+  const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
+
+  return (
+    <div className="w-full font-sans md:px-10" ref={containerRef}>
+      <h2 className="pl-20 pr-4 font-sans text-2xl font-bold text-transparent md:text-5xl bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-600 md:pl-4 text-start">
+        Pracovné skúsenosti
+      </h2>
+      <div ref={ref} className="relative pb-20 mx-auto max-w-7xl">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className="flex justify-start pt-5 md:pt-20 md:gap-5"
+          >
+            <div className="sticky z-40 flex flex-col items-center self-start max-w-xs md:flex-row top-40 lg:max-w-sm md:w-full">
+              <div className="absolute flex items-center justify-center w-10 h-10 bg-white rounded-full left-3 md:left-3 dark:bg-black">
+                <div className="w-4 h-4 p-2 border rounded-full bg-neutral-200 dark:bg-neutral-800 border-neutral-300 dark:border-neutral-700" />
+              </div>
+              <h3 className="hidden text-xl font-bold md:block md:pl-20 md:text-5xl text-neutral-500 dark:text-neutral-500 text-start">
+                {item.title}
+              </h3>
+            </div>
+
+            <div className="relative w-full pl-20 pr-4 md:pl-4">
+              <h3 className="block mb-4 text-2xl font-bold text-left md:hidden text-neutral-500 dark:text-neutral-500">
+                {item.title}
+              </h3>
+              {item.content}{" "}
+            </div>
+          </div>
+        ))}
+        <div
+          style={{
+            height: height + "px",
+          }}
+          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+        >
+          <motion.div
+            style={{
+              height: heightTransform,
+              opacity: opacityTransform,
+            }}
+            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/workSection.jsx
+++ src/components/workSection.jsx
@@ -1,6 +1,6 @@
-import { Timeline } from "@/components/ui/timeline";
+import { Timeline } from "./ui/timeline";
 
-export default function Index() {
+const WorkSection = () => {
   const timelineData = [
     {
       title: "2024",
@@ -9,7 +9,6 @@ export default function Index() {
           <h2 className="mb-4 font-sans text-2xl font-bold text-transparent leading-1 md:text-5xl/[55px] bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-600 text-start hover:bg-clip-text ">
             <a
               href="https://www.kupujauto.sk/"
-              target="_blank"
               className="hover:bg-clip-text hover:bg-no-repeat hover:text-transparent hover:bg-gradient-to-r  hover:from-purple-500 hover:via-violet-500 hover:to-pink-500 hover:[text-shadow:0_0_rgba(0,0,0,0.1)]"
             >
               kupujauto.sk
@@ -54,7 +53,6 @@ export default function Index() {
           <h2 className="mb-4 font-sans text-2xl font-bold text-transparent leading-1 md:text-5xl/[55px] bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-600 text-start hover:bg-clip-text ">
             <a
               href="https://www.nelaprovazi.cz/"
-              target="_blank"
               className="hover:bg-clip-text hover:bg-no-repeat hover:text-transparent hover:bg-gradient-to-r  hover:from-purple-500 hover:via-violet-500 hover:to-pink-500 hover:[text-shadow:0_0_rgba(0,0,0,0.1)]"
             >
               nelaprovazi.cz
@@ -83,46 +81,7 @@ export default function Index() {
       ),
     },
   ];
+  return <Timeline data={timelineData} />;
+};
 
-  return (
-    // <BackgroundBeamsWithCollision>
-    <main className="container px-4 text-center m:px-0 text-neutral-500">
-      <section className="my-10 md:my-20">
-        <h1 className="font-sans text-3xl font-bold text-transparent md:text-7xl bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-600">
-          Radovan Zeman
-        </h1>
-        <p className="text-sm">
-          <a
-            href="https://github.com/radozeman"
-            target="_blank"
-            className="hover:bg-clip-text hover:bg-no-repeat hover:text-transparent hover:bg-gradient-to-r  hover:from-purple-500 hover:via-violet-500 hover:to-pink-500 hover:[text-shadow:0_0_rgba(0,0,0,0.1)]"
-          >
-            Github
-          </a>{" "}
-          |{" "}
-          <a
-            href="mailto:r.zeman96@gmail.com"
-            target="_blank"
-            className="hover:bg-clip-text hover:bg-no-repeat hover:text-transparent hover:bg-gradient-to-r  hover:from-purple-500 hover:via-violet-500 hover:to-pink-500 hover:[text-shadow:0_0_rgba(0,0,0,0.1)]"
-          >
-            r.zeman96@gmail.com
-          </a>{" "}
-          |{" "}
-          <a
-            href="tel:+421915357238"
-            target="_blank"
-            className="hover:bg-clip-text hover:bg-no-repeat hover:text-transparent hover:bg-gradient-to-r  hover:from-purple-500 hover:via-violet-500 hover:to-pink-500 hover:[text-shadow:0_0_rgba(0,0,0,0.1)]"
-          >
-            +421 915 357 238
-          </a>{" "}
-          | Bratislava | Slovakia
-        </p>
-      </section>
-      <section></section>
-      <section className="flex flex-col items-start">
-        <Timeline data={timelineData} />
-      </section>
-    </main>
-    // </BackgroundBeamsWithCollision>
-  );
-}
+export default WorkSection;
