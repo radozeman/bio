c0f96c611e206498d2bb7e2e6a333e1401ecf7b1
radozeman <r.zeman96@gmail.com>
2024-09-27 01:50:24 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Header from "@/components/Header";
-
 import WorkSection from "@/components/workSection";
 
 export default function Index() {
   return (
     <main className="container px-4 text-center m:px-0 text-neutral-500">
       <Header />
-
       <section className="flex flex-col items-start">
         <WorkSection />
       </section>
